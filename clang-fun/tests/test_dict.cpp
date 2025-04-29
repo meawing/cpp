1373ade07f45d77b69bf4a51ff2a7a0eea103ec1
Aleksandr Kukovitskiy <98482634+meawing@users.noreply.github.com>
2025-04-29 23:52:43 +0300

    #

--- clang-fun/tests/test_dict.cpp
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-#include "util.h"
-
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("Dict") {
-    auto dir = GetFileDir(__FILE__) / "dict";
-    CheckDir(dir , dir / "dict.txt");
-}
