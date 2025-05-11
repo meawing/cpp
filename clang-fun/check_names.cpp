120ee62a65fa3cabea3942ba6e3b495ef80d4e09
Aleksandr Kukovitskiy <98482634+meawing@users.noreply.github.com>
2025-05-11 23:51:34 +0300

    #

--- clang-fun/check_names.cpp
+++
@@ -1,9 +0,0 @@
-#include "../check_names.h"
-#include <clang/AST/ASTConsumer.h>
-#include <clang/AST/RecursiveASTVisitor.h>
-#include <clang/Frontend/CompilerInstance.h>
-#include <clang/Frontend/FrontendAction.h>
-#include <clang/Tooling/CommonOptionsParser.h>
-#include <clang/Tooling/Tooling.h>
-#include <clang/Basic/SourceManager.h>
-#include <llvm/Support/CommandLine.h>
